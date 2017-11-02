be3ad71c4d20ceb2d30fa3a3df034bb62e4d1774
Ulises Rosas <ulisesfrosasp@gmail.com>
2017-11-02 04:55:07 +0000

    Add files via upload

---
+++ oneline.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my $input_fasta = $ARGV[0];
+
+unless(open(IN, "$input_fasta")){
+	print "\nError upon opening \"$input_fasta\"\n\n";
+	exit;
+	}
+
+my @lines  = <IN>;
+close IN;
+
+my $line =  join("", @lines);
+my $results = "";
+
+for $line (@lines){
+	if($line =~ m/^>/){
+	$results .=  "\n$line"; 
+	}else{
+	$line =~ s/\s//g;
+	$results .=  $line;
+	}
+}
+
+$results =~ s/^\n//g;
+print "$results\n";
+exit;
